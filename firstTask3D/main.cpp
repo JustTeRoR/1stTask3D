8bc29f505a4eef356497cbabf347ac6290eeb01f
JustTeRoR <pavlenok2000@gmail.com>
2022-06-13 13:21:15 +0300

    Initial Commit

---
+++ firstTask3D/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  firstTask3D
+//
+//  Created by Сергей Павленок on 13.06.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
